52f59ceecd5f0153fadcb20c7ea4c2e78b9c84f4
Michael Rowsey <mike@rowsey.org>
2025-07-20 22:52:14 -0700

    save todos at add, edit, complete

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ if not os.path.exists(filepath):
 else:
     with open(filepath, 'r') as f:
         todos = json.load(f)
-
 f.close()
 
 while True:
@@ -19,16 +18,23 @@ while True:
         case 'add' | 'a':
             todo = input("Enter Todo : ")
             todos.append(todo)
+            with open(filepath, "w") as f:
+                json.dump(todos, f)
+            f.close()
+            print(f"Todo added.")
         case 'show' | 's':
             for index, todo in enumerate(todos):
                 print(f"{index + 1}. {todo.title()}")
         case 'edit' | 'e':
-            index = int(input("Enter ToDo to edit : "))
+            index = int(input("Which ToDo do you want to edit? "))
             if index - 1 < 0 or index - 1 >= len(todos):
                 print("Index out-of-range. Please try again.")
             else:
                 todo = input("Enter Todo : ")
                 todos[index - 1] = todo
+                with open(filepath, "w") as f:
+                    json.dump(todos, f)
+                f.close()
                 print(f"Todo #{index} updated.")
         case 'complete' | 'c':
             index = int(input("Enter ToDo to complete : "))
@@ -36,14 +42,14 @@ while True:
                 print("Index out-of-range. Please try again.")
             else:
                 todos.pop(index - 1)
+                with open(filepath, "w") as f:
+                    json.dump(todos, f)
+                f.close()
                 print(f"Todo #{index} complete.")
         case 'quit' | 'q':
             break
         case _:
             print("Invalid input. Please try again.")
-
-with open(filepath, "w") as f:
-    json.dump(todos, f)
 
 print("Goodbye!")
 
