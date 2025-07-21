e33152f9185001805a3f480cbb664796f8f0c7a1
Michael Rowsey <mike@rowsey.org>
2025-07-20 19:01:01 -0700

    add complete

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ prompt = "Enter To-Do : "
 todos = ["cook", "clean", "shop"]
 
 while True:
-    user_action = input("Type '(a)dd', '(s)how', '(e)dit', or '(q)uit': ").strip().lower()
+    user_action = input("Type '(a)dd', '(s)how', '(e)dit', (c)omplete, or '(q)uit': ").strip().lower()
 
     match user_action:
         case 'add' | 'a':
@@ -20,9 +20,18 @@ while True:
                 todo = input(prompt)
                 todos[index - 1] = todo
                 print(f"Todo #{index} updated.")
+        case 'complete' | 'c':
+            index = int(input("Enter ToDo to complete : "))
+            if index - 1 < 0 or index - 1 >= len(todos):
+                print("Index out-of-range. Please try again.")
+            else:
+                todos.remove(todos[index - 1])
+                print(f"Todo #{index} complete.")
         case 'quit' | 'q':
             break
         case _:
             print("Invalid input. Please try again.")
 
 print("Goodbye!")
+
+
