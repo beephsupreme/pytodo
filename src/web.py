cb6943aa027a195d701edadb893312f3dd25c349
Michael Rowsey <mike@tokistar.com>
2025-08-04 10:57:36 -0700

    move to uv based project

--- src/web.py
+++ src/web.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import functions as fn
 
+def add_todo():
+    todo = st.session_state["new_todo"]
+    print(todo)
+
 todos = fn.load_store()
 
 st.title("PyTodo")
@@ -11,5 +15,6 @@ enumerated_todos = [f"{i + 1}. {item}" for i, item in enumerate(todos)]
 for et in enumerated_todos:
     st.checkbox(et)
 
-st.text_input(label="Enter a todo")
+st.text_input(label="", placeholder="Enter a todo",
+              on_change=add_todo(), key="new_todo")
 
