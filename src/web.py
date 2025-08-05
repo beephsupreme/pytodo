a1ad6fe0600fa5427edf542dc2b8cb7ebdcaf9a7
Michael Rowsey <mike@tokistar.com>
2025-08-04 19:57:39 -0700

    bug fix

--- src/web.py
+++ src/web.py
@@ -3,7 +3,8 @@ import functions as fn
 
 def add_todo():
     todo = st.session_state["new_todo"]
-    print(todo)
+    fn.add(todo)
+    st.session_state["new_todo"] = ""
 
 todos = fn.load_store()
 
@@ -11,10 +12,16 @@ st.title("PyTodo")
 st.subheader("A simple task manager.")
 st.write("Made to increase your productivity.")
 
-enumerated_todos = [f"{i + 1}. {item}" for i, item in enumerate(todos)]
-for et in enumerated_todos:
-    st.checkbox(et)
+enumerated_todos = [f"{i}. {item}" for i, item in enumerate(todos)]
+for i, et in enumerate(enumerated_todos):
+    checkbox = st.checkbox(et)
+    if checkbox:
+        fn.complete(i)
+        todos = fn.load_store()
+        st.rerun()
 
-st.text_input(label="", placeholder="Enter a todo",
-              on_change=add_todo(), key="new_todo")
+
+
+st.text_input(label=" ", placeholder="Enter a todo",
+              on_change=add_todo, key='new_todo')
 
